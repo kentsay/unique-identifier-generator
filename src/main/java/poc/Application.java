dd42fe36b1beaa9da883909b3745ae8530901b53
Ken Tsay <ken.tsay@careem.com>
2017-07-26 10:08:04 +0200

    apply code review comments

--- src/main/java/poc/Application.java
+++ src/main/java/poc/Application.java
@@ -16,37 +16,42 @@ import poc.repository.SeqPrefixedUserRepo;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
-    }
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(Application.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.run(args);
+  }
 
-    @Bean
-    public CommandLineRunner demo(BaselineUserRepo baselineUserRepo, PrefixedUserRepo prefixedUserRepo, SeqPrefixedUserRepo seqPrefixedUserRepo) {
-        return (args) -> {
-            if (args.length < 1 || args.length > 2) {
-                System.out.println("Usage: java -jar poc-id-generator-<version>.jar baseline|prefixed|table [number of iterations]");
-                return;
-            }
+  @Bean
+  public CommandLineRunner demo(BaselineUserRepo baselineUserRepo,
+      PrefixedUserRepo prefixedUserRepo, SeqPrefixedUserRepo seqPrefixedUserRepo) {
+    return (args) -> {
+      if (args.length < 1 || args.length > 2) {
+        System.out.println(
+            "Usage: java -jar poc-id-generator-<version>.jar baseline|prefixed|table [number of iterations]");
+        return;
+      }
 
-            long maxIterations = 1000L;
-            if (args.length == 2) {
-                maxIterations = Long.parseLong(args[1]);
-            }
+      long maxIterations = 1000L;
+      if (args.length == 2) {
+        maxIterations = Long.parseLong(args[1]);
+      }
 
-            switch (args[0]) {
-                case "baseline":
-                    LongStream.rangeClosed(1L, maxIterations).forEach(c -> baselineUserRepo.save(new BaselineUser(Long.toString(c))));
-                    break;
-                case "prefixed":
-                    LongStream.rangeClosed(1L, maxIterations).forEach(c -> prefixedUserRepo.save(new PrefixedUser(Long.toString(c))));
-                    break;
-                case "table":
-                    LongStream.rangeClosed(1L, maxIterations).forEach(c -> seqPrefixedUserRepo.save(new SeqPrefixedUser(Long.toString(c))));
-                    break;
-            }
-        };
-    }
+      switch (args[0]) {
+        case "baseline":
+          LongStream.rangeClosed(1L, maxIterations)
+              .forEach(c -> baselineUserRepo.save(new BaselineUser(Long.toString(c))));
+          break;
+        case "prefixed":
+          LongStream.rangeClosed(1L, maxIterations)
+              .forEach(c -> prefixedUserRepo.save(new PrefixedUser(Long.toString(c))));
+          break;
+        case "table":
+          LongStream.rangeClosed(1L, maxIterations)
+              .forEach(c -> seqPrefixedUserRepo.save(new SeqPrefixedUser(Long.toString(c))));
+          break;
+      }
+    };
+  }
 
 }
--- src/main/java/poc/repository/SeqPrefixedUserRepo.java
+++ src/main/java/poc/repository/SeqPrefixedUserRepo.java
@@ -3,8 +3,6 @@ package poc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import poc.model.SeqPrefixedUser;
 
-/**
- * Created by kentsay on 25.07.17.
- */
 public interface SeqPrefixedUserRepo extends JpaRepository<SeqPrefixedUser, Long> {
+
 }
