adf25ce02ae8543d30e971a16a038991c4bd51ab
Ken Tsay <ken.tsay@careem.com>
2017-07-27 11:02:37 +0200

    script to run performance testing

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for strategy in baseline custPrefixed seqPrefixed 
+do
+  for numThread in 1 10 50 100
+  do
+    for numIteration in 200 400 600 800 1000
+    do 
+      java -jar target/poc-id-generator-1.0-SNAPSHOT.jar $strategy $numThread $numIteration
+    done
+  done
+done
